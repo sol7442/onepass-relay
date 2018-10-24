ad922069fc2807d8ebf48e4de6174b07758c3f27
sol7442 <yongsung.kim@gmail.com>
2018-10-24 15:31:25 +0900

    change arc 1.0.0 --> 1.0.1

---
+++ onepass-relay-api/src/main/java/com/raonsecure/op/relay/OnePassResult.java
@@ -0,0 +1,41 @@
+package com.raonsecure.op.relay;
+
+public class OnePassResult {
+	private String trId;
+	private String expireTime;
+	private String trStatus;
+	
+	private String trStatusMsg;
+	private OnePassDevice[] aaidAllowList;
+	
+	public String getTrId() {
+		return trId;
+	}
+	public void setTrId(String trId) {
+		this.trId = trId;
+	}
+	public String getExpireTime() {
+		return expireTime;
+	}
+	public void setExpireTime(String expireTime) {
+		this.expireTime = expireTime;
+	}
+	public String getTrStatus() {
+		return trStatus;
+	}
+	public void setTrStatus(String trStatus) {
+		this.trStatus = trStatus;
+	}
+	public String getTrStatusMsg() {
+		return trStatusMsg;
+	}
+	public void setTrStatusMsg(String trStatusMsg) {
+		this.trStatusMsg = trStatusMsg;
+	}
+	public OnePassDevice[] getAaidAllowList() {
+		return aaidAllowList;
+	}
+	public void setAaidAllowList(OnePassDevice[] aaidAllowList) {
+		this.aaidAllowList = aaidAllowList;
+	}
+}
