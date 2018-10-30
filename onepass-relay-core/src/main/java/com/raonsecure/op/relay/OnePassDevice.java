81d8f39113bd9c9305c8f91a162eed0dcfca86c5
sol <yongsung.kim@gmail.com>
2018-10-31 01:26:41 +0900

    a

--- onepass-relay-core/src/main/java/com/raonsecure/op/relay/OnePassDevice.java
+++ onepass-relay-core/src/main/java/com/raonsecure/op/relay/OnePassDevice.java
@@ -2,7 +2,37 @@ package com.raonsecure.op.relay;
 
 public class OnePassDevice {
     private String aaid;					
-    private String verificationType;					
+    public String getAaid() {
+		return aaid;
+	}
+	public void setAaid(String aaid) {
+		this.aaid = aaid;
+	}
+	public String getVerificationType() {
+		return verificationType;
+	}
+	public void setVerificationType(String verificationType) {
+		this.verificationType = verificationType;
+	}
+	public String getVerificationNm() {
+		return verificationNm;
+	}
+	public void setVerificationNm(String verificationNm) {
+		this.verificationNm = verificationNm;
+	}
+	public String getVendorNm() {
+		return vendorNm;
+	}
+	public void setVendorNm(String vendorNm) {
+		this.vendorNm = vendorNm;
+	}
+	public String getVendorId() {
+		return vendorId;
+	}
+	public void setVendorId(String vendorId) {
+		this.vendorId = vendorId;
+	}
+	private String verificationType;					
     private String verificationNm;					
     private String vendorNm;				
     private String vendorId;					
